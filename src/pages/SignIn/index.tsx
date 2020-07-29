830a1c368a33c11c5e916a8e14344eaaba99f71a
doravanteBeto <roberto.araujo339@gmail.com>
2020-07-29 11:59:21 -0300

    Input & Button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-import {Container} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {Container, Title} from './styles';
 
 import logo from '../../assets/logo.png';
 
@@ -9,6 +12,10 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
